141ab4fe58ba31d5190ed691733cc37c91660370
edusalam <eduarfernando.salcedo@gmail.com>
2025-06-15 22:40:56 -0500

    actualizado

---
+++ Practica.py
@@ -0,0 +1,21 @@
+#estructura de control
+'''
+eduard = 10
+
+if eduard ==67:
+    print('el valor es verdadero')
+if eduard == 10:
+    print('el valar es 10')
+
+eduard = 10
+if eduard % 2 == 0:
+    print('el resto es cero')
+
+else: 
+    print('el resto no es cero')
+'''
+lista = [1,2,3,4,5,6]
+indice = 0
+while indice < len(lista):
+    print(lista[indice])
+    indice +=1
