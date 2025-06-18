709f72c0dcbddcd6e929c728f7e36c6fcdc6a1cc
edusalam <eduarfernando.salcedo@gmail.com>
2025-06-17 23:23:37 -0500

    actu

--- Practica.py
+++ Practica.py
@@ -13,9 +13,38 @@ if eduard % 2 == 0:
 
 else: 
     print('el resto no es cero')
-'''
+
 lista = [1,2,3,4,5,6]
 indice = 0
 while indice < len(lista):
     print(lista[indice])
     indice +=1
+
+diccionario = {'eduard':'amaya','estudiantes':'genios'}
+type(diccionario)
+#diccionario['eduard']
+
+apilar = [1,2,3,4]
+apilar.append(5)
+apilar
+
+from collections import deque
+colas = deque()
+colas
+
+ingreso = input('ingrese tu clase')
+
+#salida por panntalla
+print('hola')
+'''
+def atras(segundos):
+    segundos -= 1
+    if segundos >=  0:
+        print(segundos)
+        atras(segundos)
+    else:
+        print('termino la cuenta atras')
+        
+
+
+
